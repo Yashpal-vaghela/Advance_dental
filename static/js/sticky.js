d69f72b456efb6d0c0b05b13560bc38e9af33e24
savitri dhameliya <shavitridhameliya@gmail.com>
2025-02-04 12:04:27 +0530

    remove unnecessary css and js in base html file and change static image jpg to webp in index page.

--- static/js/sticky.js
+++ static/js/sticky.js
@@ -1,18 +1,10 @@
 // Create a clone of the menu, right next to original.
 $('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
-
 scrollIntervalID = setInterval(stickIt, 10);
-
-
 function stickIt() {
-
   var orgElementPos = $('.original').offset();
   orgElementTop = orgElementPos.top;               
-
-  if ($(window).scrollTop() >= (orgElementTop)) {
-    // scrolled past the original position; now only show the cloned, sticky element.
-
-    // Cloned element should always have same left position and width as original element.     
+  if ($(window).scrollTop() >= (orgElementTop)) {     
     orgElement = $('.original');
     coordsOrgElement = orgElement.offset();
     leftOrgElement = coordsOrgElement.left;  
