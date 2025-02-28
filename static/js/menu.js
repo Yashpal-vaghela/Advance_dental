d9c9a14caea8a3317fecdd4d084e579991a6dcbe
savitri dhameliya <shavitridhameliya@gmail.com>
2025-02-28 18:24:22 +0530

    integrate responsive exhibition gallery card design and some navbar changes

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,26 +1,17 @@
-/* global jQuery */
-/* global document */
-
 jQuery(function () {
   'use strict';
-
   document.addEventListener("touchstart", function () {}, false);
   jQuery(function () {
-
     jQuery('body').wrapInner('<div class="wsmenucontainer" />');
     jQuery('<div class="overlapblackbg"></div>').prependTo('.wsmenu');
-
     jQuery('#wsnavtoggle').click(function () {
       jQuery('body').toggleClass('wsactive');
     });
-
     jQuery('.overlapblackbg').click(function () {
       jQuery("body").removeClass('wsactive');
     });
-
     jQuery('.wsmenu > .wsmenu-list > li').has('.sub-menu').prepend('<span class="wsmenu-click"><i class="wsmenu-arrow"></i></span>');
     jQuery('.wsmenu > .wsmenu-list > li').has('.wsmegamenu').prepend('<span class="wsmenu-click"><i class="wsmenu-arrow"></i></span>');
-
     jQuery('.wsmenu-click').click(function () {
       jQuery(this).toggleClass('ws-activearrow')
         .parent().siblings().children().removeClass('ws-activearrow');
@@ -28,17 +19,13 @@ jQuery(function () {
       jQuery(this).siblings('.sub-menu').slideToggle('slow');
       jQuery(this).siblings('.wsmegamenu').slideToggle('slow');
     });
-
     jQuery('.wsmenu > .wsmenu-list > li > ul > li').has('.sub-menu').prepend('<span class="wsmenu-click02"><i class="wsmenu-arrow"></i></span>');
     jQuery('.wsmenu > .wsmenu-list > li > ul > li > ul > li').has('.sub-menu').prepend('<span class="wsmenu-click02"><i class="wsmenu-arrow"></i></span>');
-
     jQuery('.wsmenu-click02').click(function () {
       jQuery(this).children('.wsmenu-arrow').toggleClass('wsmenu-rotate');
       jQuery(this).siblings('li > .sub-menu').slideToggle('slow');
     });
-
     jQuery(window).on('resize', function () {
-
       if (jQuery(window).outerWidth() < 992) {
         jQuery('.wsmenu').css('height', jQuery(this).height() + "px");
         jQuery('.wsmenucontainer').css('min-width', jQuery(this).width() + "px");
@@ -50,10 +37,7 @@ jQuery(function () {
         jQuery('.wsmenu-click').removeClass("ws-activearrow");
         jQuery('.wsmenu-click02 > i').removeClass("wsmenu-rotate");
       }
-
     });
-
     jQuery(window).trigger('resize');
-
   });
 }());
