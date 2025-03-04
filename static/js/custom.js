1a9157cd042a8794ce85e86ca55a24cec463086a
Yashpal-vaghela <vaghela9632@gmail.com>
2025-03-04 12:41:24 +0530

    gallery page error sloved

--- static/js/custom.js
+++ static/js/custom.js
@@ -265,39 +265,39 @@ $(document).ready(function() {
 	/*	Portfolio Grid
 	/*----------------------------------------------------*/
 
-	// $('.grid-loaded').imagesLoaded(function () {
+	$('.grid-loaded').imagesLoaded(function () {
 
-	// 	// filter items on button click
-	// 	$('.gallery-filter').on('click', 'button', function () {
-	// 		var filterValue = $(this).attr('data-filter');
-	// 		$grid.isotope({
-	// 		  filter: filterValue
-	// 		});
-	// 	});
+		// filter items on button click
+		$('.gallery-filter').on('click', 'button', function () {
+			var filterValue = $(this).attr('data-filter');
+			$grid.isotope({
+			  filter: filterValue
+			});
+		});
 
-	// 	// change is-checked class on buttons
-	// 	$('.gallery-filter button').on('click', function () {
-	// 		$('.gallery-filter button').removeClass('is-checked');
-	// 		$(this).addClass('is-checked');
-	// 		var selector = $(this).attr('data-filter');
-	// 		$grid.isotope({
-	// 		  filter: selector
-	// 		});
-	// 		return false;
-	// 	});
+		// change is-checked class on buttons
+		$('.gallery-filter button').on('click', function () {
+			$('.gallery-filter button').removeClass('is-checked');
+			$(this).addClass('is-checked');
+			var selector = $(this).attr('data-filter');
+			$grid.isotope({
+			  filter: selector
+			});
+			return false;
+		});
 
-	// 	// init Isotope
-	// 	var $grid = $('.masonry-wrap').isotope({
-	// 		itemSelector: '.gallery-item',
-	// 		percentPosition: true,
-	// 		transitionDuration: '0.7s',
-	// 		masonry: {
-	// 		  // use outer width of grid-sizer for columnWidth
-	// 		  columnWidth: '.gallery-item',
-	// 		}
-	// 	});
+		// init Isotope
+		var $grid = $('.masonry-wrap').isotope({
+			itemSelector: '.gallery-item',
+			percentPosition: true,
+			transitionDuration: '0.7s',
+			masonry: {
+			  // use outer width of grid-sizer for columnWidth
+			  columnWidth: '.gallery-item',
+			}
+		});
 		
-	// });
+	});
 
 
 	/*----------------------------------------------------*/
