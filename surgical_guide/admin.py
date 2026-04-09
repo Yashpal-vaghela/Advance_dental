423e3563484d243f2146c9a23da7a069e3c31b21
Yashpal-vaghela <vaghela9632@gmail.com>
2026-04-09 16:57:33 +0530

    form validation done

--- surgical_guide/admin.py
+++ surgical_guide/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import SurgicalGuide
 
-# Register your models here.
+
+class SurgicalGuideAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'preferred_implant_company',
+        'implant_name',
+        'created_at',
+    )
+
+    search_fields = (
+        'preferred_implant_company',
+        'implant_name',
+        'desired_implant_location',
+    )
+
+
+admin.site.register(SurgicalGuide, SurgicalGuideAdmin)
--- surgical_guide/models.py
+++ surgical_guide/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class SurgicalGuide(models.Model):
+    preferred_implant_company = models.CharField(max_length=255, blank=True, null=True)
+    implant_name = models.CharField(max_length=255, blank=True, null=True)
+    JAW_CHOICES = [
+        ('maxilla', 'Maxilla'),
+        ('mandible', 'Mandible'),
+    ]
+    jaw_information = models.JSONField(blank=True, null=True)
+    prosthetic_plan = models.TextField(blank=True, null=True)
+    desired_implant_location = models.CharField(max_length=255, blank=True, null=True)
+    preferred_size = models.CharField(max_length=255, blank=True, null=True)
+    ap_spread_angle_implant = models.CharField(max_length=255, blank=True, null=True)
+    immediate_extractions = models.BooleanField(null=True, blank=True)
+    immediate_extractions_notes = models.CharField(max_length=255, blank=True, null=True)
+    bone_grafting = models.BooleanField(null=True, blank=True)
+    bone_grafting_notes = models.CharField(max_length=255, blank=True, null=True)
+    bone_reduction = models.BooleanField(null=True, blank=True)
+    bone_reduction_notes = models.CharField(max_length=255, blank=True, null=True)
+    surgical_drill_kit = models.CharField(max_length=255, blank=True, null=True)
+    provisional_planning = models.TextField(blank=True, null=True)
+    surgery_date = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Surgical Guide #{self.id}"
+    
